be39a9c02f1166c948c99998c4b284a7f6556bbe
IrfanSujarin <sujarinirfan@gmail.com>
2023-06-13 10:23:59 +0800

    initial commit

---
+++ src/penilaian/Mahasiswa.java
@@ -0,0 +1,29 @@
+
+package penilaian;
+
+public class Mahasiswa {
+    //variabel
+    private String npm, nama;
+    
+    //constructor
+    public Mahasiswa(String npm, String nama, String prodi){
+        this.npm = npm;
+        this.nama = nama;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+}
